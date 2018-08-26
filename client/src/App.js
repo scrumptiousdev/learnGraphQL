012fbdc2ffd21d22e32b5b32976a54e1c36c3a7f
Ryan Choi <ryan.mingyu.choi@gmail.com>
2018-08-25 18:03:31 -0700

    Client for React app and Server for GraphQL UI tool

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+const TodosQuery = gql`{
+    todos {
+        id
+        text
+        complete
+    }
+}`;
+
+class App extends Component {
+    render() {
+        const {data: { loading, todos }} = this.props;
+        if (loading) {
+            return null;
+        }
+        return (
+            <div>
+                {todos.map(todo => (<div key={`${todo.id}-todo-item`}>{todo.text}</div>))}
+            </div>
+        );
+    }
+}
+
+export default graphql(TodosQuery)(App);
